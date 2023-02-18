8461df5f846b47a0f3bd2b913184b648e7c628b4
TeaCupp <nastyalivesindream@gmail.com>
2023-02-18 16:33:50 +0200

    Find praviance

--- lab1.py
+++ lab1.py
@@ -6,8 +6,10 @@ print(f'Відсортований список: {sorted(grades)}')
 
 print(f'Середнє значення, раховане ручним методом: {sum(grades)/len(grades)}')
 
-print(f'Середнє значення, раховане за допомогою модуля statistics:{statistics.mean(grades)}')
+print(f'Середнє значення, раховане за допомогою модуля statistics: {statistics.mean(grades)}')
 
-print(f'Медіана, рахована за допомогою модуля statistics:{statistics.median(grades)}')
+print(f'Медіана, рахована за допомогою модуля statistics: {statistics.median(grades)}')
 
-print(f'Мода, рахована за допомогою модуля statistics:{statistics.mode(grades)}')
+print(f'Мода, рахована за допомогою модуля statistics: {statistics.mode(grades)}')
+
+print(f'Дисперсією генеральної сукупності: {statistics.pvariance([1, 3, 4, 2, 6, 5, 3, 4, 5, 2])}')
